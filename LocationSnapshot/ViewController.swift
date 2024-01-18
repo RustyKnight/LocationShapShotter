52f18860b2f1a4f45393d7108061ef27a69fa963
Shane Whitehead <obishiro@gmail.com>
2024-01-19 10:46:47 +1100

    Add support for Look Around Scene snap shotter Change minimum target support to iOS 17+ Add support for async/await request Update authorisation workflow

--- LocationSnapshot/ViewController.swift
+++ LocationSnapshot/ViewController.swift
@@ -13,7 +13,8 @@ import MapKit
 class ViewController: UIViewController {
 
 	@IBOutlet weak var mapPreviewImageView: UIImageView!
-	
+    @IBOutlet weak var scenePreviewImageView: UIImageView!
+
 	var locationManager = CLLocationManager()
 	
 	override func viewDidLoad() {
@@ -23,68 +24,94 @@ class ViewController: UIViewController {
 		mapPreviewImageView.layer.borderColor = UIColor.darkGray.cgColor
 		mapPreviewImageView.layer.borderWidth = 1
 		mapPreviewImageView.clipsToBounds = true
+
+        scenePreviewImageView.layer.cornerRadius = 20
+        scenePreviewImageView.layer.borderColor = UIColor.darkGray.cgColor
+        scenePreviewImageView.layer.borderWidth = 1
+        scenePreviewImageView.clipsToBounds = true
 	}
 	
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
 		
-		locationManager.requestWhenInUseAuthorization()
-		guard CLLocationManager.locationServicesEnabled() else {
-			return
-		}
-		
-		locationManager.delegate = self
-		locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
-		locationManager.startUpdatingLocation()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.delegate = self
+        
+        guard locationManager.authorizationStatus.isAuthorised else {
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+        locationManager.startUpdatingLocation()
 	}
-	
-	func snapshot(_ location: CLLocation) {
-		let options = MKMapSnapshotter.Options()
-		options.region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
-		options.showsBuildings = true
-		options.showsPointsOfInterest = true
-		let queue = DispatchQueue.global(qos: .userInitiated)
-		let snapshotter = MKMapSnapshotter(options: options)
-		print("Generate image")
-		snapshotter.start(with: queue) { (snapshot, error) in
-			print("Process image")
-			guard error == nil else {
-				print("!! \(error!)")
-				return
-			}
-			guard let snapshotImage = snapshot?.image,
-				let coordinatePoint = snapshot?.point(for: location.coordinate),
-				let pinImage = UIImage(named: "Beer") else {
-				print("!! 💀")
-				return
-			}
-			print("Drop pin")
-			print(snapshotImage.size)
-			UIGraphicsBeginImageContextWithOptions(snapshotImage.size, true, snapshotImage.scale)
-			snapshotImage.draw(at: CGPoint.zero)
-			
-			let fixedPinPoint = CGPoint(x: coordinatePoint.x - pinImage.size.width / 2, y: coordinatePoint.y - pinImage.size.height)
-			pinImage.draw(at: fixedPinPoint)
-			
-			guard let mapImage = UIGraphicsGetImageFromCurrentImageContext() else {
-				print("!! 💀☹️")
-				return
-			}
-			
-			DispatchQueue.main.async {
-				self.mapPreviewImageView.image = mapImage
-			}
-		}
-	}
+    
+    func snapshotMapAt(_ location: CLLocation) async throws -> UIImage? {
+        let options = MKMapSnapshotter.Options()
+        options.region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+        options.showsBuildings = true
+        
+        let config = MKStandardMapConfiguration()
+        config.pointOfInterestFilter = MKPointOfInterestFilter(excluding: [])
+        options.preferredConfiguration = config
+        
+        let snapshot = try await MKMapSnapshotter(options: options).start()
+        let snapshotImage = snapshot.image
+        let coordinatePoint = snapshot.point(for: location.coordinate)
+        
+        guard let pinImage = UIImage(named: "Beer") else {
+            return nil
+        }
+
+        UIGraphicsBeginImageContextWithOptions(snapshotImage.size, true, snapshotImage.scale)
+        snapshotImage.draw(at: CGPoint.zero)
+        
+        let fixedPinPoint = CGPoint(x: coordinatePoint.x - pinImage.size.width / 2, y: coordinatePoint.y - pinImage.size.height)
+        pinImage.draw(at: fixedPinPoint)
+        
+        guard let mapImage = UIGraphicsGetImageFromCurrentImageContext() else {
+            return nil
+        }
+        
+        return mapImage
+    }
+    
+    func snapshotStreetViewAt(_ location: CLLocation) async throws -> UIImage? {
+        guard let scene = try await MKLookAroundSceneRequest(coordinate: location.coordinate).scene else { return nil }
+        let options = MKLookAroundSnapshotter.Options()
+        options.size = CGSize(width: 256, height: 256)
+        let image = try await MKLookAroundSnapshotter(scene: scene, options: options).snapshot
+        return image.image
+    }
 
 }
 
+extension CLAuthorizationStatus {
+    var isAuthorised: Bool {
+        switch self {
+        case .notDetermined, .restricted, .denied: return false
+        case .authorizedAlways, .authorizedWhenInUse: return true
+        }
+    }
+}
+
 extension ViewController: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .notDetermined, .restricted, .denied:
+            break
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
 	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
 		guard let location = manager.location else { return }
 		
 		locationManager.stopUpdatingLocation()
-		snapshot(location)
+        
+        Task {
+            mapPreviewImageView.image = try await snapshotMapAt(location)
+            scenePreviewImageView.image = try await snapshotStreetViewAt(location)
+        }
 	}
 }
 
